0206f8fe043e988dc414f2cfd59bf1186ab4dced
Anthony Jefferson G. de Moura <anthonyjeff17@gmail.com>
2020-02-12 16:21:36 -0300

    Update main.py

--- SelectionSort/main.py
+++ SelectionSort/main.py
@@ -1,45 +1,60 @@
-import timeit
-from random import randint
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+from random import randint, shuffle
+import timeit
 
-def geraLista(tam):
-    lista = []
-    while len(lista) < tam:
-        n = randint(1,1*tam)
-        if n not in lista: lista.append(n)
-    return lista
-
-def geraGraf(x,BSort,xl = "Entradas", yl = "loops",nam="img"):
-    fig = plt.figure(figsize=(10, 13))
+def plot_grafico(x, y, z, file_name, label1, label2, xl = "Entradas", yl = "Saídas"):
+    fig = plt.figure(figsize=(10, 8))
     ax = fig.add_subplot(111)
-    ax.plot(x,BSort, label = "Bubble Sort")
+    ax.plot(x, y, label= label1)
+    ax.plot(x, z, label= label2)
     ax.legend(bbox_to_anchor=(1, 1),bbox_transform=plt.gcf().transFigure)
     plt.ylabel(yl)
     plt.xlabel(xl)
-    plt.savefig(nam)
 
-def bubbleSort(array):
+    fig.savefig(file_name)
+
+def geraLista(tam):
+    lista = list(range(1, tam+1))
+    shuffle(lista)
+    return lista
+
+def selection_sort(lista):
     count = 0
-    n = len(array)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            count+=1
-            if array[j] > array[j+1] :
-                array[j], array[j+1] = array[j+1], array[j]
+    flag = False
+    for i in range(len(lista)):
+        min = i
+        for j in range(i + 1, len(lista)):
+            count += 1
+            if lista[min] > lista[j]:
+                min = j
+
+        aux = lista[i]
+        lista[i] = lista[min]
+        lista[min] = aux
+
+
     return count
-  
-lista = [1000,10000,30000,60000]
-saidaB = []
-saidaL = []
 
+x = [1000, 10000, 30000, 60000]
+y = []
+y_des = []
+tempo_ord = []
+count_ord = []
+tempo_des = []
+count_des = []
 
-for i in range(len(lista)):
-  saidaB.append(timeit.timeit("bubbleSort({})".format(geraLista(lista[i])),setup="from __main__ import geraLista,bubbleSort",number=1))
+for i in range(len(x)):
+    y.append(geraLista(x[i]))
+    count_ord.append(selection_sort(y[i]))
+    aux = list(range(1, x[i]))
+    aux.reverse()
+    y_des.append(aux)
+    count_des.append(selection_sort(y_des[i]))
 
-geraGraf(lista,saidaB,nam="graf1")
+for i in range(len(x)):
+    tempo_ord.append(timeit.timeit("selection_sort({})".format(y[i]), setup="from __main__ import selection_sort", number=1))
+    tempo_des.append(timeit.timeit("selection_sort({})".format(y_des[i]), setup="from __main__ import selection_sort", number=1))
 
-for i in range(len(lista)):
-
-  saidaL.append(bubbleSort(geraLista(lista[i])))
-
-geraGraf(lista,saidaL,nam="graf2")
+plot_grafico(x, tempo_ord, tempo_des, "Tempo.png", "Tempo Embaralhado", "Tempo Invertido")
+plot_grafico(x, count_ord, count_des, "Interacoes.png", "Numero de Interacoes Embaralhado", "Numero de Interacoes Invertido")
